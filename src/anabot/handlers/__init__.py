1271a59ae1bf210dd58e68b03cc0645b84eef5f3
Artyom Sosedka <sostema@gmail.com>
2022-10-08 17:57:45 +0300

    fix: proper import handlers

--- src/anabot/handlers/__init__.py
+++ src/anabot/handlers/__init__.py
@@ -1,5 +1,5 @@
-from .users import *  # noqa
-from .errors import *  # noqa
+from .errors import error_handler  # noqa
+from .users import commands, echo, help, news, registration, start  # noqa
 
 
 def setup_handlers(dp):
--- src/anabot/loader.py
+++ src/anabot/loader.py
@@ -1,11 +1,11 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from anabot.data import config
+import anabot.handlers as handlers
 from anabot.middlewares import setup_middleware
-from anabot.handlers import setup_handlers
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 setup_middleware(dp)
-setup_handlers(dp)
+handlers.setup_handlers(dp)
