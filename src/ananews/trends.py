f6a181fe54d977b5f47fbcbb3a6498885e493b96
Andrey Laptev <55894714+AndreyML@users.noreply.github.com>
2022-10-09 06:54:19 +0300

    Update trends.py

--- src/ananews/trends.py
+++ src/ananews/trends.py
@@ -2,19 +2,19 @@ from sklearn.cluster import MiniBatchKMeans
 import numpy as np
 
 class Trends:
-    def __init__(self, n_clusters=5, text_data=None, id_data=None):
+    def __init__(self, n_clusters=5, emb_data=None, id_data=None):
         self.n_clusters = n_clusters
-        self.text_data = text_data
+        self.emb_data = emb_data
         self.id_data = id_data
-        self.clusterer = MiniBatchKMeans(n_clusters=self.n_clusters).fit(self.text_data)
+        self.clusterer = MiniBatchKMeans(n_clusters=self.n_clusters).fit(self.emb_data)
         self.labels = dict(zip(self.id_data, self.clusterer.labels_))
 
     def get_trends_label(self, emb):
         label = self.clusterer.predict(emb)
         return label
 
-    def update_clusters(self, text_data, id_data):
-        self.text_data = text_data
+    def update_clusters(self, emb_data, id_data):
+        self.emb_data = emb_data
         self.id_data = id_data
-        self.clusterer = MiniBatchKMeans(n_clusters=self.n_clusters).fit(self.text_data)
+        self.clusterer = MiniBatchKMeans(n_clusters=self.n_clusters).fit(self.emb_data)
         self.labels = dict(zip(self.id_data, self.clusterer.labels_))
